236d782783eb6ed90aaa52c3655f9d194dd2ba0b
Lucas Caparelli <lucas.caparelli112@gmail.com>
2020-05-07 11:28:08 -0300

    Remove unnecessary watch logic for secrets (#50)
    Signed-off-by: Lucas Caparelli <lucas.caparelli112@gmail.com>

--- pkg/controller/nexus/resource/resources.go
+++ pkg/controller/nexus/resource/resources.go
@@ -18,12 +18,8 @@
 package resource
 
 import (
-	"context"
 	"fmt"
 	"github.com/m88i/nexus-operator/pkg/cluster/kubernetes"
-	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"reflect"
 
 	"github.com/RHsyseng/operator-utils/pkg/resource/read"
@@ -50,11 +46,6 @@ type NexusResourceManager interface {
 	GetDeployedResources(nexus *v1alpha1.Nexus) (resources map[reflect.Type][]resource.KubernetesResource, err error)
 	// CreateRequiredResources will create the requests resources as it's supposed to be
 	CreateRequiredResources(nexus *v1alpha1.Nexus) (resources map[reflect.Type][]resource.KubernetesResource, err error)
-}
-
-type resourceA interface {
-	runtime.Object
-	v12.ObjectMetaAccessor
 }
 
 type nexusResourceManager struct {
@@ -148,9 +139,6 @@ func (r *nexusResourceManager) createIngress(nexus *v1alpha1.Nexus, service *v1.
 	builder := (&ingressBuilder{}).newIngress(nexus, service)
 
 	if len(nexus.Spec.Networking.TLS.SecretName) > 0 {
-		if err := r.updateWatchedSecret(nexus); err != nil {
-			return nil, err
-		}
 		builder = builder.withCustomTLS()
 	}
 
@@ -179,109 +167,4 @@ func (r *nexusResourceManager) createRoute(nexus *v1alpha1.Nexus, service *v1.Se
 		return nil, fmt.Errorf("couldn't create route: %v", err)
 	}
 	return route, nil
-}
-
-func (r *nexusResourceManager) updateWatchedSecret(nexus *v1alpha1.Nexus) error {
-	secret := &v1.Secret{}
-	objectKey := types.NamespacedName{Name: nexus.Spec.Networking.TLS.SecretName, Namespace: nexus.Namespace}
-
-	err := r.client.Get(context.TODO(), objectKey, secret)
-	if err != nil {
-		return fmt.Errorf("couldn't fetch Secret (%s): %v", objectKey.Name, err)
-	}
-
-	oldIngress := &v1beta1.Ingress{}
-	objectKey = types.NamespacedName{Namespace: nexus.Namespace, Name: nexus.Name}
-
-	err = r.client.Get(context.TODO(), objectKey, oldIngress)
-	if err != nil {
-		// there is no previous Ingress deployed, so we only need to take ownership of the secret
-		if errors.IsNotFound(err) {
-			err = r.ownNewObject(nexus, nil, secret)
-			if err != nil {
-				return fmt.Errorf("couldn't take ownership of new secret: %v", err)
-			}
-		} else {
-			return fmt.Errorf("couldn't fetch deployed Ingress (%s): %v", objectKey.Name, err)
-		}
-	}
-	// Previously deployed Ingress has no TLS configuration, so we only need to take ownership of the secret
-	if len(oldIngress.Spec.TLS) == 0 {
-		err = r.ownNewObject(nexus, nil, secret)
-		if err != nil {
-			return fmt.Errorf("couldn't take ownership of new secret: %v", err)
-		}
-	}
-	// Previously deployed Ingress already has TLS configuration, but it's not using the same secret
-	// We need to remove ownership of previous secret and add to new one
-	if len(oldIngress.Spec.TLS) == 1 && oldIngress.Spec.TLS[0].SecretName != nexus.Spec.Networking.TLS.SecretName {
-		oldSecret := &v1.Secret{}
-		objectKey := types.NamespacedName{Name: oldIngress.Spec.TLS[0].SecretName, Namespace: nexus.Namespace}
-
-		err := r.client.Get(context.TODO(), objectKey, oldSecret)
-		if err != nil {
-			if !errors.IsNotFound(err) {
-				return fmt.Errorf("coulnd't fetch previous secret: %v", err)
-			} else {
-				// The previous secret has been deleted, so we don't need to adjust it
-				err = r.ownNewObject(nexus, nil, secret)
-				if err != nil {
-					return fmt.Errorf("couldn't take ownership of new secret: %v", err)
-				}
-			}
-		}
-		err = r.ownNewObject(nexus, oldSecret, secret)
-		if err != nil {
-			return fmt.Errorf("couldn't take ownership of new secret: %v", err)
-		}
-	}
-	return nil
-}
-
-func (r *nexusResourceManager) ownNewObject(nexus *v1alpha1.Nexus, oldObject, newObject resource.KubernetesResource) error {
-	if oldObject != nil {
-		err := removeOwnership(nexus, oldObject)
-		if err != nil {
-			log.Warnf("Failed to remove ownership from old secret: %v. Has it been tampered with?", err)
-		}
-	}
-
-	if !isOwner(nexus, newObject) {
-		newObject.SetOwnerReferences(append(newObject.GetOwnerReferences(), ownerReference(nexus)))
-		err := r.client.Update(context.TODO(), newObject)
-		if err != nil {
-			return fmt.Errorf("couldn't add the Operator as owner for the secret (%s): %v", newObject.GetName(), err)
-		}
-	}
-	return nil
-}
-
-func removeOwnership(nexus *v1alpha1.Nexus, object resource.KubernetesResource) error {
-	for i, ref := range object.GetOwnerReferences() {
-		if nexus.UID == ref.UID {
-			object.SetOwnerReferences(append(object.GetOwnerReferences()[:i], object.GetOwnerReferences()[i+1:]...))
-			return nil
-		}
-	}
-	return fmt.Errorf("nexus/%s was not an owner of secret/%s", nexus.Name, object.GetName())
-}
-
-func isOwner(nexus *v1alpha1.Nexus, object resource.KubernetesResource) bool {
-	for _, ref := range object.GetOwnerReferences() {
-		if nexus.UID == ref.UID {
-			return true
-		}
-	}
-	return false
-}
-
-func ownerReference(nexus *v1alpha1.Nexus) v12.OwnerReference {
-	isController := true
-	return v12.OwnerReference{
-		UID:        nexus.UID,
-		Name:       nexus.Name,
-		APIVersion: nexus.APIVersion,
-		Kind:       nexus.Kind,
-		Controller: &isController,
-	}
 }
